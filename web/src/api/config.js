a7d33bf7e4a7afd2e39a7ca80ac917c256ba7191
aihuanhuan <44722523+ahh666@users.noreply.github.com>
2021-04-16 11:03:24 +0800

    前端配置更新
    1. 生产环境打包关闭ourceMap
    2. 生产环境接口服务修改为本地服务

--- web/src/api/config.js
+++ web/src/api/config.js
@@ -2,9 +2,9 @@
  * @Description: Description
  * @Author: 艾欢欢<ahh666@qq.com>
  * @Date: 2021-02-04 13:43:04
- * @LastEditTime: 2021-03-19 10:23:09
+ * @LastEditTime: 2021-04-15 17:33:57
  * @LastEditors: 艾欢欢<ahh666@qq.com>
- * @FilePath: \mock-server\web\src\api\config.js
+ * @FilePath: \web\src\api\config.js
  */
 const env = process.env.NODE_ENV;
 
@@ -13,7 +13,7 @@ const config = {
     baseUrl: "http://127.0.0.1:8088",
   },
   production: {
-    baseUrl: "https://api.ahh.yananart.cn",
+    baseUrl: "http://127.0.0.1:8088",
   },
 };
 
--- web/vue.config.js
+++ web/vue.config.js
@@ -2,11 +2,13 @@
  * @Description: Description
  * @Author: 艾欢欢<ahh666@qq.com>
  * @Date: 2021-03-18 19:42:36
- * @LastEditTime: 2021-03-18 19:46:44
+ * @LastEditTime: 2021-04-16 11:01:24
  * @LastEditors: 艾欢欢<ahh666@qq.com>
- * @FilePath: \mock-server\web\vue.config.js
+ * @FilePath: \web\vue.config.js
  */
+const isProd = process.env.NODE_ENV === "production";
 module.exports = {
+  productionSourceMap: !isProd,
   devServer: {
     port: 8001,
   },
